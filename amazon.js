bdd9adc2dcd3f6fe712c7f586631cfc7e80560bf
Ozone <mohammaddr653@gmail.com>
2022-07-26 01:56:57 +0430

    amazon header commit 5

--- amazon.js
+++ amazon.js
@@ -1,5 +1,6 @@
 const locChoose = document.getElementById('loc-choose');
 const nationChoose = document.getElementById('nation-choose');
+const headerSignIn = document.getElementById('header-sign-in');
 const BodyBlackBackGround= document.getElementById('body-background-black-screen');
 const HeaderBlackBackGround= document.getElementById('header-background-black-screen');
 function locChooseOpen(){
@@ -15,14 +16,25 @@ function locChooseClose(){
         HeaderBlackBackGround.style.display="none";
     }
 }
-// function nationChooseOpen(){
-//     nationChoose.style.display="block";
-//     BodyBlackBackGround.style.display="block";
-//     HeaderBlackBackGround.style.display="block";
+function nationChooseOpen(){
+    nationChoose.style.display="block";
+    BodyBlackBackGround.style.display="block";
+    // HeaderBlackBackGround.style.display="block";
 
-// }
-// function nationChooseClose(){
-//         nationChoose.style.display="none";
-//         BodyBlackBackGround.style.display="none";
-//         HeaderBlackBackGround.style.display="none";
-// }
+}
+function nationChooseClose(){
+        nationChoose.style.display="none";
+        BodyBlackBackGround.style.display="none";
+        // HeaderBlackBackGround.style.display="none";
+}
+function headerSignOpen(){
+    headerSignIn.style.display="block";
+    BodyBlackBackGround.style.display="block";
+    // HeaderBlackBackGround.style.display="block";
+
+}
+function headerSignClose(){
+        headerSignIn.style.display="none";
+        BodyBlackBackGround.style.display="none";
+        // HeaderBlackBackGround.style.display="none";
+}
